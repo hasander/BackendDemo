44fb8e3d5c673dd0570ce6e6f0b1dcc8c0896162
hasander <hsnder@gmail.com>
2022-05-15 16:11:15 +0300

    Change TokenDTO

--- BackendDemo.Core/DTOs/Auth/TokenDTO.cs
+++ BackendDemo.Core/DTOs/Auth/TokenDTO.cs
@@ -3,7 +3,5 @@
 public class TokenDTO
 {
     public string AccessToken { get; set; } = default!;
-    public DateTime AccesTokenExpirationDate { get; set; }
-    public string RefreshToken { get; set; } = default!;
-    public DateTime RefreshTokenExpirationDate { get; set; }
+    public DateTime AccesTokenExpirationDate { get; set; } 
 }
